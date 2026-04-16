703df4fbc85363c82d770e93f9e1299a16888cff
way322 <iliyamalihin@yandex.ru>
2026-04-16 20:07:29 +0300

    first commitasdaasasdsda

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'http://sd.railway.internal/api',
 });
 
 // Добавляем токен в заголовки, если он есть
